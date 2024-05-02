1a1a5a053073d905ef5e85ca6334a7bbeb4179f8
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-05-02 16:08:41 +0600

    get user backend

---
+++ TodoBackend/controller/userController.js
@@ -0,0 +1,16 @@
+const User = require("../models/userModel");
+const userController = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const user = await User.findById(id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "user Not Found" });
+    }
+    return res.status(200).json({ status: "success", data: user });
+  } catch (err) {
+    return res.status(404).send({ status: "Not Found", message: err.message });
+  }
+};
+module.exports = userController;
--- TodoBackend/router/api/index.js
+++ TodoBackend/router/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const authRoute = require("./auth");
-const taskRoute = require(".//task");
+const taskRoute = require("./task");
+const userRoute = require("./user");
 
 router.use("/auth", authRoute);
 router.use("/task", taskRoute);
+router.use("/user", userRoute);
 
 module.exports = router;
